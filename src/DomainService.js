aab788137f50058468864c45991a8d98889a2d61
Yuichi Tanikawa <t.yuichi111@gmail.com>
2016-12-09 01:35:49 +0900

    Drop storage permission

--- src/DomainService.js
+++ src/DomainService.js
@@ -43,18 +43,6 @@ export default class DomainService {
     return domain.replace(/^.*?:\/\//, '').split('/')[0];
   }
 
-  static persist(domains = []) {
-    return new Promise((resolve, reject) => {
-      chrome.storage.sync.set({ allowedDomains: domains }, () => {
-        if (chrome.runtime.lastError) {
-          reject(chrome.runtime.lastError.message);
-          return;
-        }
-        resolve();
-      });
-    });
-  }
-
   get domains() {
     return this[domainsKey];
   }
@@ -66,25 +54,25 @@ export default class DomainService {
 
   add(domain) {
     return DomainService.addPermission(domain)
-      .then(() => DomainService.persist(this.domains.concat(domain)))
       .then(() => this.domains.push(domain));
   }
 
   deleteAt(index) {
     const remainingDomains = this.domains.filter((_, i) => i !== index);
     return DomainService.deletePermission(this.domains[index])
-      .then(() => DomainService.persist(remainingDomains))
       .then(() => (this.domains = remainingDomains));
   }
 
   restore() {
     return new Promise((resolve, reject) => {
-      chrome.storage.sync.get({ allowedDomains: [] }, (items) => {
+      chrome.permissions.getAll(({ origins = [] }) => {
         if (chrome.runtime.lastError) {
           reject(chrome.runtime.lastError.message);
           return;
         }
-        this.domains = items.allowedDomains;
+        if (origins.length > 0) {
+          this.domains = origins.map(DomainService.normalize);
+        }
         resolve();
       });
     });
